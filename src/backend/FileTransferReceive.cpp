09e07c3814a7e3b5b55c41a3a75f981a1601a2e3
dr|z3d <z3d@i2pmail.org>
2020-07-01 10:21:08 +0000

    ETA wording

--- src/backend/FileTransferReceive.cpp
+++ src/backend/FileTransferReceive.cpp
@@ -393,7 +393,7 @@ void CFileTransferReceive::CalcETA(int speed) {
 
   if (secLeft > 86400) {
     //> 24h
-    emit signETA(tr("never or (>24 h)"));
+    emit signETA(tr("Over a day..."));
   } else {
     int hours = 0;
     int minutes = 0;
