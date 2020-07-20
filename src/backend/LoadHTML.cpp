6bb4b6eb8e2c1ff3466836d21f421d742b4ee828
dr|z3d <z3d@i2pmail.org>
2020-07-20 17:32:12 +0000

    Cleanups

--- src/backend/LoadHTML.cpp
+++ src/backend/LoadHTML.cpp
@@ -31,24 +31,13 @@
 using namespace std;
 
 QString gethttpheader(QString pagedata) {
-  /*const QString HTTPAGE_CSP =
-      ""
-      "X-Frame-Options: DENY\r\n"
-      "Content-Security-Policy: default-src 'self'; style-src 'none'; "
-      "script-src 'none'; form-action 'none'; frame-ancestors 'none'; "
-      "object-src 'none'; media-src 'none'; script-sample: '<script>'\r\n"
-      "X-XSS-Protection: 1; mode=block\r\n"
-      "X-Content-Type-Options: nosniff\r\n"
-      "";
-*/
-  QString header="HTTP/1.0 200\r\nContent-Length: " + QString::number(pagedata.size()) + "\r\n" +
+  QString header;
+  header =
+      "HTTP/1.0 200\r\nContent-Length: " + QString::number(pagedata.size()) + "\r\n" +
       "Content-Security-Policy: default-src 'none'; style-src 'unsafe-inline'; img-src data:\r\n" +
       "X-XSS-Protection: 1; mode=block\r\n" +
       "X-Content-Type-Options: nosniff\r\n" +
       "Content-Type: text/html; charset=utf-8\r\n\r\n";
-
-  //header = "HTTP/1.0 200\r\n" + HTTPAGE_CSP +
-     //      "Content-Length: " + QString::number(pagedata.size()) + "\r\n\r\n";
   return header;
 }
 
