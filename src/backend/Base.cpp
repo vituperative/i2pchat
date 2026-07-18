686301c81d8f070fea9817f87e9e2b0885840b58
dr|z3d <z3d@i2pmail.org>
2026-07-18 14:19:27 +0000

    fix: remove unused T32 base32 table from Base.cpp

--- src/backend/Base.cpp
+++ src/backend/Base.cpp
@@ -5,11 +5,6 @@
 
 namespace i2p {
 namespace data {
-static const char T32[32] = {
-  'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
-  'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '2', '3', '4', '5', '6', '7',
-};
-
 static void iT64Build();
 
 /*
